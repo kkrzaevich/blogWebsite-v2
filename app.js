fe466e819326a93c9bed4a554c8ecb718ef134b8
Konstantin Korolev <konstantinrzaevich@gmail.com>
2023-02-12 09:25:26 +0300

    Have protected variables

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 //jshint esversion:6
 
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+mongoose.connect(`mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@cluster76232.ammm8wy.mongodb.net/blogDB`);
+
+// Create schema
+
+const postSchema = { title: String, content: String };
+
+// Create model
+
+const BlogPost = mongoose.model('BlogPost', postSchema);
+
+// НОВЫЕ МОДУЛИ
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,52 +29,53 @@ let pageName = '';
 
 app.set('view engine', 'ejs');
 
-
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
+  // ВОТ ЗДЕСЬ ПОДКЛЮЧИТЬСЯ К БАЗЕ ДАННЫХ
+  let posts = [];
+
+
+
 
 app.get(`/*`, (req, res) => {
   // console.log(req._parsedOriginalUrl.href.substring(1));
   if (req.originalUrl.includes('favicon.ico')) {
     res.status(204).end()
   } else if (req.originalUrl.includes('posts/')) {
+    // Находим исходные посты и помещаем в массив с постами
+    // НУЖНО ПОСЛАТЬ В КАЧЕСТВЕ URL ID
     pageName = "post";
-    const postName = _.kebabCase(req.originalUrl.substring(7));
-    // console.log(postName);
+
+    // const postName = _.kebabCase(req.originalUrl.substring(7));
     let sentPost;
-    posts.forEach(function(item) {
-      // console.log(_.kebabCase(item.title));
-      if (item.titleKebab === postName) { 
-        console.log ("Match found!");
-        sentPost = item;
-      } 
-      else { console.log ("Match not found :(")}
-    }) 
-    res.render('mainview', {ejsPageName: pageName, ejsPost: sentPost});
+    BlogPost.find({_id: req.originalUrl.substring(7)}, function(err,items) {
+      sentPost = items[0];
+      console.log('SENT POST:')
+      console.log(sentPost)
+      res.render('mainview', {ejsPageName: pageName, ejsPost: sentPost});
+    });
   }
   else {
       pageName = req._parsedOriginalUrl.href.substring(1);
     if (pageName === '') {
       pageName = "home";
     }
-    res.render('mainview', {ejsHomeStartingContent: homeStartingContent, ejsAboutContent: aboutContent, ejsContactContent: contactContent, ejsPageName: pageName, ejsPosts: posts});
-  }
+    BlogPost.find({}, function(err,items) {
+      posts = items;
+      res.render('mainview', {ejsHomeStartingContent: homeStartingContent, ejsAboutContent: aboutContent, ejsContactContent: contactContent, ejsPageName: pageName, ejsPosts: posts});
+    });  }
   })
 
 app.post('/compose', (req,res) => {
+  // const post = {title: req.body.newTitle, content: req.body.newPost, titleKebab: _.kebabCase(req.body.newTitle)};
+  // ВОТ ПОДКЛЮЧИТЬСЯ К БАЗЕ ДАННЫХ
+  // posts.push(post);
+  // ВОТ ЗДЕСЬ ЗАСУНУТЬ ФАЙЛЫ В БАЗУ ДАННЫХ
+  const newPost = new BlogPost({ title: req.body.newTitle, content: req.body.newPost });
+  // console.log(newPost);
+  newPost.save();
   res.redirect('/home');
-  // nodeItem = req.body.newItem;
-  // nodeItems.push(nodeItem);
-  // console.log(nodeItems);
-  // console.log(req.body.newTitle);
-  // console.log(req.body.newPost);
-  const post = {title: req.body.newTitle, content: req.body.newPost, titleKebab: _.kebabCase(req.body.newTitle)};
-  posts.push(post);
-  // console.log(post);
-  // console.log(posts);
 });
 
 app.listen(3000, function(req) {
